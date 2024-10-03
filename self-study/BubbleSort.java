e25f1c2f126a3d461f5458c5c9183a4499c0a2c0
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-10-03 18:29:21 +0530

    bubble sort code added

---
+++ self-study/BubbleSort.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void printArray(int[] arr) {
+        
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "  ");
+        }
+        
+        System.out.println();
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+            
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+    
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    
+	    System.out.print("Enter Size of the array: ");
+        
+		int n = sc.nextInt();
+		
+        System.out.println("Enter array Elements");
+		int[] arr = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+		    arr[i] = sc.nextInt();
+		}
+		
+		System.out.println("Array Elements before Sorting");
+		printArray(arr);
+		
+		bubbleSort(arr);
+		
+		System.out.println("Array Elements before Sorting");
+		printArray(arr);
+	}
+}
