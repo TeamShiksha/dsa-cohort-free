c2ad87d55ec1d67ac8706b964a90d140a1854ffa
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-10-03 19:19:31 +0530

    quick sort code added

---
+++ official/java/QuickSort.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "  ");
+        }
+        System.out.println();
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        if (i != j) {
+            arr[i] = arr[i] ^ arr[j];
+            arr[j] = arr[i] ^ arr[j];
+            arr[i] = arr[i] ^ arr[j];
+        }
+    }
+
+    public static int partition(int[] arr, int start, int end) {
+        int pivot = arr[start];
+        int i = start;
+        
+        for (int j = start + 1; j <= end; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        
+        swap(arr, start, i);
+        return i;
+    }
+
+    public static void quicksort(int[] arr, int start, int end) {
+        if (start < end) {
+            int pivotIndex = partition(arr, start, end);
+            quicksort(arr, start, pivotIndex - 1);
+            quicksort(arr, pivotIndex + 1, end);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        quicksort(arr, 0, n - 1);
+        printArray(arr);
+    }
+}
