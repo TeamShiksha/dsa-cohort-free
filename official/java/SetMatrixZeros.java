7fe3356f73a6ba282ed77d1fd49d952ae1ee58db
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-10-15 18:11:34 +0530

    Set Matrix Zeros optimized solution code added

--- official/java/SetMatrixZeros.java
+++ official/java/SetMatrixZeros.java
@@ -1,6 +1,44 @@
 // Question Link: https://www.interviewbit.com/problems/set-matrix-zeros/
 
 
+// Brute Force Solution - TC = O(n^2), SC = O(1)
+public class Solution {
+    public void setZeroes(ArrayList<ArrayList<Integer>> matrix) {
+        boolean firstColZero = false;
+        
+        int n = matrix.size();        
+        int m = matrix.get(0).size(); 
+
+        for (int i = 0; i < n; i++) {
+            if (matrix.get(i).get(0) == 0) {
+                firstColZero = true; 
+            }
+            
+            for (int j = 1; j < m; j++) {
+                if (matrix.get(i).get(j) == 0) {
+                    matrix.get(i).set(0, 0); 
+                    matrix.get(0).set(j, 0); 
+                }
+            }
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 1; j--) {
+                if (matrix.get(i).get(0) == 0 || matrix.get(0).get(j) == 0) {
+                    matrix.get(i).set(j, 0); // Zero the cell
+                }
+            }
+            
+            if (firstColZero) {
+                matrix.get(i).set(0, 0);
+            }
+        }
+    }
+}
+
+
+/*
+// Brute Force Solution - TC = O(n^2), SC = O(m + n)
 public class Solution {
     public void setZeroes(ArrayList<ArrayList<Integer>> a) {
         int rows = a.size();
@@ -36,3 +74,4 @@ public class Solution {
         }
     }
 }
+*/
